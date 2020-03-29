17138ed2819b38093898b4f36c11350c23b6ce09
Duy Nguyen Huu <duy.nguyen-huu@wize-solutions.com>
2020-03-29 17:16:22 +0700

    update to use node8

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 module.exports = {
-
-    sqlConnectionString: "<connection-string-for-your-sql-database-goes-here>", // Insert your connection string here.
+    sqlConnectionString: "Server=.;Database=AdventureWorksLT2017;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}",
     mongoConnectionString: "mongodb://localhost:27017", // This puts the resulting database in MongoDB running on your local PC.
-    targetDatabaseName: "target-database", // Specify the MongoDB database where the data will end up.
+    targetDatabaseName: "AdventureWorksLT2017", // Specify the MongoDB database where the data will end up.
     skip: [
-        "sql-table-to-skip-1", // Add the tables here that you don't want to replicate to MongoDB.
-        "sql-table-to-skip-2"
-    ],
-    remapKeys: true // Set this to false if you want to leave table keys as they are, set to true to remap them to MongoDB ObjectId's.
+        //"sql-table-to-skip-1", // Add the tables here that you don't want to replicate to MongoDB.
+    ]
 };
